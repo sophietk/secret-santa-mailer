cfa02a2cd4ae517d495ccd733ff4c3e79feb3f68
sophietk <sophie.tk@gmail.com>
2024-12-13 18:01:16 +0100

    build(deps): switch to esmodule and bump mocha, chai

--- test/index.js
+++ test/index.js
@@ -1,30 +1,24 @@
 /* eslint-disable no-unused-expressions */
 
-const { describe, it } = require('mocha')
-const { should } = require('chai')
+import { describe, it } from 'mocha'
+import 'chai/register-should.js'
 
-const santa = require('../lib')
-
-should()
+import { getParticipants, setParticipants, randomize } from '../lib/index.js'
 
 describe('santa', () => {
-  it('should exist', () => {
-    santa.should.not.be.undefined
-  })
-
   describe('getParticipants', () => {
     it('should return participants', () => {
-      santa.getParticipants().should.be.an('array').that.is.empty
+      getParticipants().should.be.an('array').that.is.empty
     })
   })
 
   describe('setParticipants', () => {
     it('should change participants', () => {
-      santa.setParticipants([
+      setParticipants([
         { name: 'Guybrush', email: 'guybrush@monkeyisland.net' },
         { name: 'Le Chuck', email: 'le-chuck@monkeyisland.net' }
       ])
-      santa.getParticipants().should.be.an('array')
+      getParticipants().should.be.an('array')
         .that.have.deep.members([
           { name: 'Guybrush', email: 'guybrush@monkeyisland.net' },
           { name: 'Le Chuck', email: 'le-chuck@monkeyisland.net' }
@@ -34,21 +28,21 @@ describe('santa', () => {
 
   describe('randomize', () => {
     it('should return an array with same number of participants', () => {
-      santa.setParticipants([
+      setParticipants([
         { name: 'Guybrush', email: 'guybrush@monkeyisland.net' },
         { name: 'Le Chuck', email: 'le-chuck@monkeyisland.net' },
         { name: 'Elaine', email: 'elaine@monkeyisland.net' }
       ])
-      santa.randomize().should.be.an('array')
+      randomize().should.be.an('array')
         .that.have.lengthOf(3)
     })
 
     it('should return a loop of santas', () => {
-      santa.setParticipants([
+      setParticipants([
         { name: 'Guybrush', email: 'guybrush@monkeyisland.net' },
         { name: 'Le Chuck', email: 'le-chuck@monkeyisland.net' }
       ])
-      santa.randomize().should.include.deep.members([
+      randomize().should.include.deep.members([
         { name: 'Guybrush', email: 'guybrush@monkeyisland.net', shouldBuyGiftFor: 'Le Chuck' },
         { name: 'Le Chuck', email: 'le-chuck@monkeyisland.net', shouldBuyGiftFor: 'Guybrush' }
       ])
